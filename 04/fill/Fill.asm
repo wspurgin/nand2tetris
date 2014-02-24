3d9780788ac4f0ab11cc41686b30d4b9c4a686ca
Will Spurgin <wspurgin@smu.edu>
2014-02-23 19:21:29 -0600

    Implemented Fill (finally holy crap)

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -9,3 +9,86 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+(WAIT)
+    @KBD
+    D=M
+
+    @BLACK
+    D;JNE
+
+    @WHITE
+    D;JEQ
+
+    @WAIT
+    0;JMP
+
+(BLACK)
+
+    @SCREEN //start of screen map
+    D=A
+
+    @pos //set position to start
+    M=D
+
+    @LOOP1
+    0;JMP
+
+    (LOOP1)
+
+
+        @pos //get current position
+        D=M
+
+        A=D //Set current pos to 'black'
+        M=-1
+
+        @pos //increment position
+        M=M+1
+
+        @24576 //check if we are at the end
+        D=A
+
+        @pos
+        D=D-M
+
+        @WAIT
+        D;JEQ
+        
+        @LOOP1
+        D;JGT
+
+(WHITE)
+
+    @SCREEN //start of screen map
+    D=A
+
+    @pos //set position to start
+    M=D
+
+    @LOOP2
+    0;JMP
+
+    (LOOP2)
+
+
+        @pos //get current position
+        D=M
+
+        A=D //Set current pos to 'white'
+        M=0
+
+        @pos //increment position
+        M=M+1
+
+        @24576 //check if we are at the end
+        D=A
+
+        @pos
+        D=D-M
+
+        @WAIT
+        D;JEQ
+        
+        @LOOP2
+        D;JGT
