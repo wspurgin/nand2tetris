894d209e3c4fe9686ac148bac357e27f912ec288
Will Spurgin <wspurgin@smu.edu>
2014-03-28 11:29:04 -0500

    Stubbed out beginnings of Translator class, added .gitignore to ignore .asm files

--- 07/translator.rb
+++ 07/translator.rb
@@ -1,9 +1,57 @@
 #!/usr/bin/env ruby
+# Author
+# 
+# 
 
+# Translator Exception class
 class TranslatorException < Exception; end
 
+# == Translator class
 class Translator
-  def initialize()
+
+  def initialize(path)
+    @files = []
+
+    # strip '.vm' extension if exists
+    path.chomp!('.vm')
+
+    # <i>Love</i> the oneline-i-ness
+    vm_path, vm_basename = File.split(path)
+
+    @asm_filename = "#{vm_path}/#{vm_basename}.asm"
+    @path = "#{vm_path}/#{vm_basename}"
+
+    get_files
+    @files.each {|file| puts "#{file} will be translated into #{@asm_filename}"}
+  end
+
+  def translate!
+    File.open(@asm_filename, "w") do |asm_file|
+      for file in @files
+        File.open(file, "r") do |vm_file|
+          lines = vm_file.readlines
+          lines.each do |line|
+            asm_file << line
+          end
+        end
+      end
+    end
+    puts "#{@asm_filename} created successfully"
+  end
+
+  def get_files
+    if Dir.exist?(@path) # get all '.vm' files in dir
+      Dir.foreach(@path) { |file|
+        if File.extname(file) == '.vm'
+          @files << @path + '/' + file
+        end
+      }
+      if @files.empty?
+        raise TranslatorException.new("No files found with \'.vm\' extension in #{path}")
+      end
+    else
+      @files = [@path + '.vm']
+    end
   end
 end
 
@@ -14,46 +62,14 @@ def args_valid?
   return ARGV.size == 1 && ((File.extname(ARGV[0]) == '.vm' && File.exist?(ARGV[0])) || Dir.exist?(ARGV[0]))
 end
 
-def get_files(path)
-  files = []
-  if Dir.exist?(path) # get all '.vm' files in dir
-    Dir.foreach(path) { |file|
-      if File.extname(file) == '.vm'
-        files << path + '/' + file
-      end
-    }
-    if files.empty?
-      raise TranslatorException.new("No files found with \'.vm\' extension in #{path}")
-    end
-  else
-    files = [path]
-  end
-  return files
-end
+
 
 abort('Usage: ./translator.rb (Prog.vm || /dir/ProgamDir), the file or directory should exist') unless args_valid?
 
-begin # messy block... definitely should refuctor to functions.
-  vm_basename = nil
-  vm_path = nil
-  
-  path = ARGV[ 0 ].dup
-  # strip ending / if there is one
-  path.sub!(/\/$/, '')
+begin
 
-  if Dir.exist?(path)
-    for p in path.match(/^.*\/(.*)$|(\w*)/).captures
-        vm_basename = p unless not p
-    end
-    vm_path = path
-  else
-    vm_basename = File.basename(ARGV[0], '.vm')
-    vm_path = ARGV[0][/^(.*)\//, 1]
-  end
-
-  asm_filename = "#{vm_path}/#{vm_basename}.asm"
-  files = get_files(path)
-  files.each {|file| puts "#{file} will be translated into #{asm_filename}"}
+  translator = Translator.new(ARGV[0].dup)
+  translator.translate!
 
 rescue Exception => e
   puts e.backtrace.join("\n")
